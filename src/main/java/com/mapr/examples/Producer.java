c836db5d26120c6adb07bd6091bbaa9b66a40a1a
Nithin Thankachan <nithinthank@gmail.com>
2019-08-31 14:37:29 -0500

    first commit

--- src/main/java/com/mapr/examples/Producer.java
+++ src/main/java/com/mapr/examples/Producer.java
@@ -6,48 +6,55 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Properties;
 
-/**
- * This producer will send a bunch of messages to topic "fast-messages". Every so often,
- * it will send a message to "slow-messages". This shows how messages can be sent to
- * multiple topics. On the receiving end, we will see both kinds of messages but will
- * also see how the two topics aren't really synchronized.
- */
 public class Producer {
-    public static void main(String[] args) throws IOException {
-        // set up the producer
-        KafkaProducer<String, String> producer;
-        try (InputStream props = Resources.getResource("producer.props").openStream()) {
-            Properties properties = new Properties();
-            properties.load(props);
-            producer = new KafkaProducer<>(properties);
-        }
-
-        try {
-            for (int i = 0; i < 1000000; i++) {
-                // send lots of messages
-                producer.send(new ProducerRecord<String, String>(
-                        "fast-messages",
-                        String.format("{\"type\":\"test\", \"t\":%.3f, \"k\":%d}", System.nanoTime() * 1e-9, i)));
-
-                // every so often send to a different topic
-                if (i % 1000 == 0) {
-                    producer.send(new ProducerRecord<String, String>(
-                            "fast-messages",
-                            String.format("{\"type\":\"marker\", \"t\":%.3f, \"k\":%d}", System.nanoTime() * 1e-9, i)));
-                    producer.send(new ProducerRecord<String, String>(
-                            "summary-markers",
-                            String.format("{\"type\":\"other\", \"t\":%.3f, \"k\":%d}", System.nanoTime() * 1e-9, i)));
-                    producer.flush();
-                    System.out.println("Sent msg number " + i);
-                }
-            }
-        } catch (Throwable throwable) {
-            System.out.printf("%s", throwable.getStackTrace());
-        } finally {
-            producer.close();
-        }
-
+  public static void main(String[] args) throws IOException {
+    // set up the producer
+    KafkaProducer<String, byte[]> producer;
+    try (InputStream props = Resources.getResource("producer.props").openStream()) {
+      Properties properties = new Properties();
+      properties.load(props);
+      producer = new KafkaProducer<>(properties);
     }
+
+
+    try {
+      byte[] fileData = Files.readAllBytes(FileSystems.getDefault().getPath("src/test.JPG"));
+      ArrayList<byte[]> allChunks = splitFile(fileData);
+      for (byte[] allChunk : allChunks) {
+        ProducerRecord<String, byte[]> data = new ProducerRecord<>("test", "test.JPG", allChunk);
+        producer.send(data);
+      }
+      System.out.println("Published file test.JPG");
+    } catch (Throwable throwable) {
+      System.out.printf("%s", throwable.getStackTrace());
+    } finally {
+      producer.close();
+    }
+
+  }
+
+  private static ArrayList<byte[]> splitFile(byte[] datum) {
+    int i, l = datum.length;
+    int block = 10240;
+    int numblocks = l / block;
+    int marker = 0;
+    byte[] chunk;
+    ArrayList<byte[]> data = new ArrayList<>();
+    for (i = 0; i < numblocks; i++) {
+      chunk = Arrays.copyOfRange(datum, marker, marker + block);
+      data.add(chunk);
+      marker += block;
+    }
+    chunk = Arrays.copyOfRange(datum, marker, l);
+    data.add(chunk);
+    data.add(null);
+    return data;
+  }
+
 }
